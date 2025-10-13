59f4b5c46d3568aebe9e35a52d0369ca0c981740
merryoncherry <ezp@merryoncherry.org>
2025-10-13 11:33:28 -0400

    Apply settings from @Aakankshathokare

--- packages/player-ui-components/src/components/playback-settings/PlaybackSettingsDrawer.tsx
+++ packages/player-ui-components/src/components/playback-settings/PlaybackSettingsDrawer.tsx
@@ -710,11 +710,18 @@ export const PlaybackSettingsDrawer: React.FC<PlaybackSettingsDrawerProps> = ({ 
                         </Typography>
                         <Slider
                             value={newVolumeScheduleEntry.volumeLevel || 100}
-                            onChange={(_, value) =>
+                            onChangeCommitted={(_, value) =>
                                 setNewVolumeScheduleEntry({ ...newVolumeScheduleEntry, volumeLevel: value as number })
                             }
                             min={0}
                             max={100}
+                            marks={[
+                                { value: 0, label: '0' },
+                                { value: 25, label: '25' },
+                                { value: 50, label: '50' },
+                                { value: 75, label: '75' },
+                                { value: 100, label: '100' },
+                            ]}
                             step={1}
                             size="small"
                             sx={{
