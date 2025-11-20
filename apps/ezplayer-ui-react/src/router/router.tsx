3d06676f4b370354786f7f1b231c54e30eb1222c
Aakankshathokare <aakanksha.thokare@indexnine.com>
2025-11-20 16:53:22 +0530

    added all the components in web server

--- apps/ezplayer-ui-react/src/router/router.tsx
+++ apps/ezplayer-ui-react/src/router/router.tsx
@@ -1,5 +1,6 @@
 import { Navigate, RouteObject } from 'react-router-dom';
-import { ShowStatusScreen, SidebarLayout, Routes as ROUTES, JukeboxScreen } from '@ezplayer/player-ui-components';
+import { ShowStatusScreen, SidebarLayout, Routes as ROUTES, JukeboxScreen, SongList, PlaylistList, PlayerScreen, SchedulePreview, Schedule, CreateEditPlaylist, PlaybackSettingsDrawer } from '@ezplayer/player-ui-components';
+import { AddSongDialogElectron } from '../../../ezplayer-ui-electron/src/components/song/AddSongDialogElectron';
 
 const getStatusArea = () => [];
 
@@ -19,7 +20,41 @@ const routes: RouteObject[] = [
             {
                 path: ROUTES.JUKEBOXSCR,
                 element: <JukeboxScreen title="Jukebox" statusArea={getStatusArea()} />,
-            }
+            },
+            {
+                path: ROUTES.SONGS,
+                element: <SongList title="Songs" AddSongDialog={AddSongDialogElectron} statusArea={getStatusArea()} />,
+            },
+            {
+                path: ROUTES.PLAYLIST,
+                element: <PlaylistList title="Playlists" statusArea={getStatusArea()} />,
+            },
+            {
+                path: ROUTES.SCHEDULE,
+                element: <Schedule title="Schedule" statusArea={getStatusArea()} />,
+            },
+            {
+                path: ROUTES.BACKGROUND_SCHEDULE,
+                element: (
+                    <Schedule title="Background Schedule" statusArea={getStatusArea()} scheduleType="background" />
+                ),
+            },
+            {
+                path: ROUTES.SCHEDULE_PREVIEW,
+                element: <SchedulePreview title="Schedule Preview" statusArea={getStatusArea()} />,
+            },
+            {
+                path: ROUTES.PLAYER,
+                element: <PlayerScreen title="Player" statusArea={getStatusArea()} />,
+            },
+            {
+                path: `${ROUTES.CREATE_EDIT_PLAYLIST}/:id`,
+                element: <CreateEditPlaylist title="unused" statusArea={getStatusArea()} />,
+            },
+            {
+                path: ROUTES.PLAYBACKSETTINGS,
+                element: <PlaybackSettingsDrawer title="Playback Settings" statusArea={getStatusArea()} />,
+            },
         ],
     },
 ];
