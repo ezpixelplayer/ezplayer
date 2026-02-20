f0a023c449fc66fd72a1beb0b0876044950fb227
merryoncherry <ezp@merryoncherry.org>
2026-02-20 15:12:28 -0500

    update layout calcs

--- packages/ezplayer-core/src/types/EZPElectronAPI.ts
+++ packages/ezplayer-core/src/types/EZPElectronAPI.ts
@@ -145,12 +145,13 @@ export interface GetNodeResult {
     logicalBufferDepth: number;
 
     /** Metadata */
+    modelType?: string;
     pixelSize?: number;
     pixelStyle?: string;
-    modelType?: string;
     pixelType?: string;
-    stringType?: string;
+    pixelSpacing?: string;
     transparency?: number;
+    stringType?: string;
 
     /** Channel mapping (populated after channel resolution) */
     channelMapping?: ModelChannelMapping;
