983ea58aed0ca3f5ff1db501b24881809fb3f199
merryoncherry <ezp@merryoncherry.org>
2026-03-08 17:10:42 -0400

    Put logos in a few places

--- apps/ezplayer-ui-embedded/src/main.tsx
+++ apps/ezplayer-ui-embedded/src/main.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { SidebarProvider } from '@ezplayer/shared-ui-components';
+import { SidebarProvider, PageHeaderLogoProvider } from '@ezplayer/shared-ui-components';
 import { initI18N } from '@ezplayer/player-ui-components';
 import 'nprogress/nprogress.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import './index.css';
+import ezplayerLogo from '../../ezplayer-ui-electron/src/assets/images/EZPlayerLogoTransparent128.png';
 
 initI18N();
+
+const headerLogo = <img src={ezplayerLogo} alt="EZPlayer" style={{ height: 28, width: 28, flexShrink: 0 }} />;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <HelmetProvider>
             <SidebarProvider>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
+                <PageHeaderLogoProvider logo={headerLogo}>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </PageHeaderLogoProvider>
             </SidebarProvider>
         </HelmetProvider>
     </React.StrictMode>,
